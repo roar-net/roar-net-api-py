bd86fa4c04fc44351c8a89af193742e9ac70b166
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-09-10 09:09:54 +0100

    Extends the previous working version for the actual TA constraints
    And also refactors the file structure.

--- examples/ta/ta.py
+++ examples/ta/ta.py
@@ -6,11 +6,11 @@
 
 from __future__ import annotations
 
+import json
 import logging
 import random
 import sys
-import json
-from collections.abc import Iterable, Sequence
+from collections.abc import Iterable
 from logging import getLogger
 from typing import Optional, Protocol, Self, TextIO, TypeVar, final
 
@@ -27,8 +27,11 @@ from roar_net_api.operations import (
     SupportsObjectiveValueIncrement,
     SupportsRandomMove,
     SupportsRandomMovesWithoutReplacement,
-    SupportsRandomSolution,
 )
+
+import ta_utils
+from ta_classes import SessionOccurrence
+from ta_classes import TeachingAssistant
 
 log = getLogger(__name__)
 
@@ -40,32 +43,18 @@ class _SupportsLT(Protocol):
 _T = TypeVar("_T", bound=_SupportsLT)
 
 
-class SessionOccurrence:
-    def __init__(self, name: str):
-        self.name = name
-
-    def __repr__(self):
-        return self.name
-
-
-class TeachingAssistant:
-    def __init__(self, name: str, qualifications: dict[SessionOccurrence, int]):
-        self.name = name
-        self.qualifications = qualifications
-
-    def __repr__(self):
-        # return "Teaching Assistant: ".join(self.name) #+ ": qualifications: ".join(self.qualifications)
-        # TODO: add qualifications to the method as well
-        return self.name
-
-
 # ---------------------------------- Solution --------------------------------
 
 
 @final
 class Solution(SupportsCopySolution, SupportsObjectiveValue, SupportsLowerBound):
-    def __init__(self, problem: Problem, mapping: dict[SessionOccurrence, TeachingAssistant],
+    def __init__(self, problem: Problem, mapping: dict[SessionOccurrence, list[TeachingAssistant]],
                  unused_tas: list[TeachingAssistant], unused_session_occurrences: list[SessionOccurrence], lb: int):
+        assert problem is not None
+        assert mapping is not None
+        assert unused_tas is not None
+        assert unused_session_occurrences is not None
+        assert lb is not None
         self.problem = problem
         self.mapping = mapping
         self.unused_tas = unused_tas
@@ -73,19 +62,71 @@ class Solution(SupportsCopySolution, SupportsObjectiveValue, SupportsLowerBound)
         self.lb = lb
 
     def __str__(self) -> str:
-        # TODO: Maybe add the unused objects here
         return " ".join(map(str, self.mapping))
 
     @property
     def is_feasible(self) -> bool:
-        return len(self.unused_session_occurrences) == 0
+        # If there are any unused/non-mapped session occurrences -> infeasible
+        if len(self.unused_session_occurrences) != 0:
+            return False
+
+        # Session occurrence checks:
+        # Check for every session occurrence that there are exactly as many TAs as needed
+        for so in self.problem.session_occurrences:
+            # If there is no mapping at all -> infeasible
+            if so not in self.mapping:
+                return False
+
+            # Check number of assigned TAs
+            if len(self.mapping[so]) != so.number_of_tas:
+                return False
+
+            # Every assigned TA must have at least a qualification != 0
+            for ta in self.mapping[so]:
+                if not so in ta.qualifications:
+                    return False
+                if ta.qualifications[so] == 0:
+                    return False
+
+        # TA checks:
+        for ta in self.problem.tas:
+            # Calculate time
+            total_time = 0
+            weekly_time = {}
+            found_session_occurrences = []
+
+            # Find working time (weekly and yearly/total) as well as assigned session occurrences
+            for so in self.mapping.keys():
+                if ta in self.mapping[so]:
+                    found_session_occurrences.append(so)
+                    total_time += so.hours_paid_per_occurrence
+                    if so.week not in weekly_time:
+                        weekly_time[so.week] = 0
+                    weekly_time[so.week] += so.hours_paid_per_occurrence
+
+            # Check weekly time
+            for w in weekly_time.keys():
+                if weekly_time[w] > ta.max_hours_per_week:
+                    return False
+
+            # Check yearly/total time
+            if total_time > ta.max_hours_per_year:
+                return False
+
+            # Check conflicting assignments
+            if ta_utils.check_occurrences_for_conflicts(found_session_occurrences):
+                return False
+
+        # If no violation was found, everything is fine and the solution is valid/feasible
+        return True
 
     def to_textio(self, f: TextIO) -> None:
+        assert f is not None
         f.write("NAME : %s\nTYPE : TA\n" % self.problem.name)
         f.write("#SessionOccurrences : %d\n" % len(self.problem.session_occurrences))
         f.write("#TeachingAssistants : %d\n" % len(self.problem.tas))
         for so in self.mapping.keys():
-            f.write(so.name + " -> " + self.mapping[so].name + "\n")
+            f.write(so.name + " -> " + " ".join(str(ta.name) + " " for ta in self.mapping[so]) + "\n")
         f.write("EOF\n")
 
     def copy_solution(self) -> Self:
@@ -106,51 +147,66 @@ class Solution(SupportsCopySolution, SupportsObjectiveValue, SupportsLowerBound)
 
 @final
 class AddMove(SupportsApplyMove[Solution], SupportsLowerBoundIncrement[Solution]):
-    def __init__(self, neighbourhood: AddNeighbourhood, session_occurrence: SessionOccurrence, ta: TeachingAssistant):
+    def __init__(self, neighbourhood: AddNeighbourhood, so: SessionOccurrence, ta: TeachingAssistant):
+        assert neighbourhood is not None
+        assert so is not None
+        assert ta is not None
+        # It is only allowed to add TAs with a qualification for the respective session occurrence
+        assert ta_utils.is_ta_qualified_for_so(ta, so)
         self.neighbourhood = neighbourhood
-        self.session_occurrence = session_occurrence
+        self.so = so
         self.ta = ta
 
     def apply_move(self, solution: Solution) -> Solution:
-        prob = solution.problem
         # Update lower bound: += qualification of the newly added TA mapping
-        solution.lb += self.ta.qualifications.get(self.session_occurrence)
+        solution.lb += self.ta.qualifications.get(self.so)
         # Update solution
-        solution.mapping[self.session_occurrence] = self.ta
-        solution.unused_session_occurrences.remove(self.session_occurrence)
+        if self.so not in solution.mapping:
+            solution.mapping[self.so] = []
+        solution.mapping[self.so].append(self.ta)
+        solution.unused_session_occurrences.remove(self.so)
         if self.ta in solution.unused_tas:
             solution.unused_tas.remove(self.ta)
         return solution
 
     def lower_bound_increment(self, solution: Solution) -> float:
-        return self.ta.qualifications.get(self.session_occurrence)
+        return self.ta.qualifications.get(self.so)
 
 
-# TODO: Rename to swap
 @final
 class NewTaMove(SupportsApplyMove[Solution], SupportsObjectiveValueIncrement[Solution]):
     def __init__(self, neighbourhood: NewTaNeighbourhood, so: SessionOccurrence, new_ta: TeachingAssistant):
+        assert neighbourhood is not None
+        assert so is not None
+        assert new_ta is not None
         # The new TA must have at least some qualification for the session occurrence
-        assert new_ta.qualifications[so] is not None
+        assert ta_utils.is_ta_qualified_for_so(new_ta, so)
         self.neighbourhood = neighbourhood
         self.so = so
-        self.ta = new_ta
+        self.new_ta = new_ta
+
+        # The old TA must be chosen randomly once
+        self.old_ta = random.choice(solution.mapping[self.so])
 
     def apply_move(self, solution: Solution) -> Solution:
-        # Update/decrement the lb by removing the previous mappings
-        solution.lb -= solution.mapping[self.so].qualifications.get(self.so)
+        # There must be at least one previously assigned TA
+        assert len(solution.mapping[self.so]) > 0
 
-        # Swap TAs
-        solution.mapping[self.so] = self.ta
+        # Update/decrement the lb by removing the previous mappings
+        solution.lb -= self.old_ta.qualifications.get(self.so)
+
+        # Replace the old TA with the new one
+        solution.mapping[self.so].remove(self.old_ta)
+        solution.mapping[self.so].append(self.new_ta)
 
         # Update lb with the new mappings
-        solution.lb += solution.mapping[self.so].qualifications.get(self.so)
+        solution.lb += self.new_ta.qualifications.get(self.so)
         return solution
 
     def objective_value_increment(self, solution: Solution) -> float:
         incr = 0
-        incr -= solution.mapping[self.so].qualifications.get(self.so)
-        incr += self.ta.qualifications.get(self.so)
+        incr -= self.old_ta.qualifications.get(self.so)
+        incr += self.new_ta.qualifications.get(self.so)
         return incr
 
 
@@ -164,14 +220,14 @@ class AddNeighbourhood(SupportsMoves[Solution, AddMove]):
 
     def moves(self, solution: Solution) -> Iterable[AddMove]:
         assert self.problem == solution.problem
-        # TODO: This statement always uses the last unused TA. I'm not sure if this makes sense in every case.
         for so in solution.unused_session_occurrences:
-            ta = solution.unused_tas[-1]
-            assert so in ta.qualifications
-            yield AddMove(self, so, ta)
+            for ta in solution.unused_tas:
+                if ta_utils.is_ta_qualified_for_so(ta, so):
+                    yield AddMove(self, so, ta)
 
 
-# TODO: Rename to swap
+# Note: the ROAR-NET API glossary states that all *local* neighbourhoods must be feasible.
+# Therefore, this neighbourhood only constructs moves that leave the solution in a feasible state.
 @final
 class NewTaNeighbourhood(
     SupportsMoves[Solution, NewTaMove],
@@ -186,28 +242,23 @@ class NewTaNeighbourhood(
         # This is only meant to be used as a local neighbourhood, so solution should be feasible
         assert solution.is_feasible
 
-        # All TAs can be assigned except if they are the same
+        # All assignable TAs should be added to the neighbourhood
         for so in self.problem.session_occurrences:
             for ta in self.problem.tas:
-                # The TA must not be the same
-                if solution.mapping[so] != ta:
-                    # The TA must have some qualifications for the session occurrence
-                    if so in ta.qualifications:
-                        yield NewTaMove(self, so, ta)
+                if ta_utils.is_ta_assignable_to_so(solution, ta, so):
+                    yield NewTaMove(self, so, ta)
 
     def random_moves_without_replacement(self, solution: Solution) -> Iterable[NewTaMove]:
         assert self.problem == solution.problem
-        sos = solution.problem.session_occurrences.copy()
-        random.shuffle(sos)
-        for idx, x in enumerate(sos):
-            tas = solution.problem.tas.copy()
-            random.shuffle(tas)
-            for idy, y in enumerate(tas):
-                # The TA must not be the same
-                if solution.mapping[x] != y:
-                    # The TA must have some qualifications for the session occurrence
-                    if x in y.qualifications:
-                        yield NewTaMove(self, x, y)
+        all_sos = solution.problem.session_occurrences.copy()
+        random.shuffle(all_sos)
+        for so in all_sos:
+            all_tas = solution.problem.tas.copy()
+            random.shuffle(all_tas)
+            for ta in all_tas:
+                # The TA must be assignable
+                if ta_utils.is_ta_assignable_to_so(solution, ta, so):
+                    yield NewTaMove(self, so, ta)
 
     def random_move(self, solution: Solution) -> Optional[NewTaMove]:
         return next(iter(self.random_moves_without_replacement(solution)), None)
@@ -221,9 +272,11 @@ class Problem(
     SupportsConstructionNeighbourhood[AddNeighbourhood],
     SupportsLocalNeighbourhood[NewTaNeighbourhood],
     SupportsEmptySolution[Solution],
-    SupportsRandomSolution[Solution],
 ):
     def __init__(self, session_occurrences: list[SessionOccurrence], tas: list[TeachingAssistant], name: str):
+        assert session_occurrences is not None
+        assert tas is not None
+        assert name is not None
         self.session_occurrences = session_occurrences
         self.tas = tas
         self.name = name
@@ -232,8 +285,7 @@ class Problem(
         self.l_nbhood: Optional[NewTaNeighbourhood] = None
 
     def __str__(self) -> str:
-        out: list[str] = []
-        out.append("Session occurrences:\n")
+        out: list[str] = ["Session occurrences:\n"]
         for so in self.session_occurrences:
             out.append(" ".join(so.name))
         out.append("Teaching assistants:\n")
@@ -253,13 +305,17 @@ class Problem(
 
     @classmethod
     def from_json(cls, path: str) -> Self:
+        assert path is not None
         with open(path) as f:
             imported = json.load(f)
 
             # Session occurrences
             imported_session_occurrences = {}
             for so in imported["sessionOccurrences"]:
-                imported_session_occurrences[so] = SessionOccurrence(so)
+                imported_session_occurrences[so["name"]] = SessionOccurrence(so["name"], so["date_start"],
+                                                                             so["date_end"], so["number_of_tas"],
+                                                                             so["hours_paid_per_occurrence"],
+                                                                             so["week"])
 
             # TAs
             imported_tas = []
@@ -267,41 +323,32 @@ class Problem(
                 leftover_session_occurrences = imported_session_occurrences.copy()
                 qualifications = {}
                 for q in ta["qualifications"].keys():
-                    # The `qualification` value must be negative because the ROAR-NET API only minimizes the obj value
+                    # The `qualification` value must be negative because the ROAR-NET API only minimizes the obj value.
+                    # (Multiplying a maximization problem with (-1) results in a minimization problem.)
                     qualifications[imported_session_occurrences[q]] = -1 * ta["qualifications"].get(q)
                     leftover_session_occurrences.pop(q)
-                # Fill all other qualifications up with '0'
+                # Fill all other (non-)qualifications up with '0'
                 for q in leftover_session_occurrences.values():
                     qualifications[q] = 0
-                new_ta = TeachingAssistant(ta["name"], qualifications)
+                new_ta = TeachingAssistant(ta["name"], qualifications, ta["max_hours_per_week"],
+                                           ta["max_hours_per_year"])
                 imported_tas.append(new_ta)
 
             return cls(list(imported_session_occurrences.values()), imported_tas, imported["name"])
 
     def empty_solution(self) -> Solution:
+        # An empty solution is just a solution with no mappings, all TAs, all session occurrences, and a lb of 0
         return Solution(self, {}, self.tas.copy(), self.session_occurrences.copy(), 0)
-
-    def random_solution(self) -> Solution:
-        mapping = {}
-        obj = 0
-        unused_tas = self.tas.copy()
-        for so in self.session_occurrences:
-            mapping[so] = random.choice(self.tas)
-            obj += mapping[so].qualifications[so]
-            unused_tas.remove(mapping[so])
-        return Solution(self, mapping.copy(), unused_tas.copy(), [], obj)
 
 
 if __name__ == "__main__":
     import roar_net_api.algorithms as alg
 
-    logging.basicConfig(stream=sys.stderr, level="INFO", format="%(levelname)s;%(asctime)s;%(message)s")
+    # Configure logging
+    logging.basicConfig(stream=sys.stdout, level="INFO", format="%(levelname)s;%(asctime)s;%(message)s")
 
+    # Load a local JSON file with a problem instance
     problem = Problem.from_json('./ta.json')
-
-    # exit(0)
-
-    # problem = Problem.from_textio(sys.stdin)
 
     # Run greedy construction to get an initial solution
     solution = alg.greedy_construction(problem)
@@ -320,4 +367,5 @@ if __name__ == "__main__":
     log.info(f"Objective value after local search: {solution.objective_value()}")
 
     # Print the final solution to stdout
+    log.info("Final solution:")
     solution.to_textio(sys.stdout)
